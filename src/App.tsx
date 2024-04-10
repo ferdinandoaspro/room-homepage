44e0cb4df8ef1bb1cc2f7f41ad259aa94e551cec
FatChickn <ferdinandoaspro2@gmail.com>
2024-04-11 01:11:49 +0200

    added conditional rendering based on vw

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import ImageSelectors from "./components/ImageSelectors.tsx"
 import NavMenu from "./components/NavMenu.tsx"
 import NavMenuOptions from "./components/NavMenuOptions.tsx"
 import useDialog from "./assets/hooks/useDialog.tsx"
+import useLayout from "./assets/hooks/useLayout.tsx"
 import {useState} from "react"
 
 function App() {
 
   const [backgroundCount, setBackgroundCount] = useState(1)
+  const layoutHook = useLayout();
   const dialogHook = useDialog();
  
   const increaseCount = () => backgroundCount === 3 ? setBackgroundCount(1) : setBackgroundCount(backgroundCount + 1)
@@ -18,19 +20,19 @@ function App() {
   return (
     <>
       <Header backgroundCount={backgroundCount} openDialog={dialogHook.openDialog}>
-        <NavMenu dialogRef={dialogHook.dialogRef} closeDialog={dialogHook.closeDialog}/>
-        <NavMenuOptions display={"max-md:hidden"}/>
-        <ImageSelectors handleIncr={increaseCount} 
+        {layoutHook.width < 786 && <NavMenu dialogRef={dialogHook.dialogRef} closeDialog={dialogHook.closeDialog}/>}
+        {layoutHook.width >= 768 && <NavMenuOptions display={"max-md:hidden"}/>}
+        {layoutHook.width < 768 && <ImageSelectors handleIncr={increaseCount} 
                         handleDecr={decreaseCount} 
                         position={"bottom-0 right-0"} 
-                        display={"md:hidden"} />
+                        display={"md:hidden"} />}
       </Header>
       <main className="font-league-spartan md:contents">
         <PrimarySection backgroundCount={backgroundCount}>
-          <ImageSelectors handleIncr={increaseCount} 
+          {layoutHook.width >= 768 && <ImageSelectors handleIncr={increaseCount} 
                           handleDecr={decreaseCount} 
                           position={"bottom-0 left-0"} 
-                          display={"max-md:hidden"} />
+                          display={"max-md:hidden"} />}
         </PrimarySection>
         <img src="/images/image-about-dark.jpg" alt="" className="w-full h-full md:col-span-3"/>
         <AboutSection />
